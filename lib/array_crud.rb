d691c44f4e6f2e7ec540293943b8d224434b8bfc
Kristyn Little <kristynlittle@frontier.com>
2021-02-06 04:33:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,6 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  dog_breeds = ["Boxer", "Jack Russell Terrier", "Golden Retriever", "Great Dane"]
   dog_breeds << "Poodle"
 end
 
